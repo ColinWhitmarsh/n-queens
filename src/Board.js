3b1c3d01f6b440c5f7a8fbffa1852ada7e13db51
ColinWhitmarsh <Colin.Whitmarsh@gmail.com>
2016-01-20 15:08:22 -0800

    Implement working hasAnyRowConflicts method

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,27 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var count = 0;
+      for(var i = 0; i < row.length; i++) {
+        if(row[i] === 1) {
+          count++;
+        }
+        if(count > 1) {
+          return true;
+        }
+      }
+      return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      var rows = this.rows();
+      for(var i = 0; i < rows.length; i++) {
+        if(this.hasRowConflictAt(i)){
+          return true;
+        }
+      }
       return false; // fixme
     },
 
